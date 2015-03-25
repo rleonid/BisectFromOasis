4af425d97be6051435db32ad120c05572fa5af0b
Leonid Rozenberg <leonidr@gmail.com>
2015-03-25 19:15:18 -0400

    Tests can run.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,7 @@
 (* OASIS_START *)
 (* OASIS_STOP *)
 open Ocamlbuild_plugin
+open Ocamlbuild_pack
 
 let has_coverage () =
   let key = "coverage=" in
@@ -18,25 +19,46 @@ let has_coverage () =
     l ()
   with _ -> false
 
-let bisect_dir () =
-  let ic = Unix.open_process_in "ocamlfind query bisect" in
+let lib_dir pkg =
+  let ic = Unix.open_process_in ("ocamlfind query " ^ pkg) in
   let line = input_line ic in
   close_in ic;
   line
+
+let rec copy_mlt_files path =
+  let elements = Pathname.readdir path in
+  Array.iter 
+    (fun p ->
+      if Pathname.is_directory (path / p) then
+        copy_mlt_files (path / p)
+      else if Pathname.check_extension p "mlt" then
+        let src = path / p in
+        let dst = !Options.build_dir / path / p in
+        Shell.mkdir_p (!Options.build_dir / path);
+        Pathname.copy src dst
+      else
+        ())
+    elements
 
 let () =
   let additional_rules = function
       | After_rules     ->
         if has_coverage () then
           begin
-            let bsdir = Printf.sprintf "%s/%s" (bisect_dir ()) in
+            let bsdir = Printf.sprintf "%s/%s" (lib_dir "bisect") in
             flag ["pp"]                        (S [A"camlp4o"; A"str.cma"; A (bsdir "bisect_pp.cmo")]);
             flag ["compile"]                   (S [A"-I"; A (bsdir "")]);
             flag ["link"; "byte"; "program"]   (S [A"-I"; A (bsdir ""); A"bisect.cmo"]);
             flag ["link"; "native"; "program"] (S [A"-I"; A (bsdir ""); A"bisect.cmx"]);
           end
         else
-          ()
+          begin
+            let kpdir = Printf.sprintf "%s/%s" (lib_dir "kaputt") in
+            copy_mlt_files "src";
+            flag ["kaputt"; "pp"]              (S [A (kpdir "kaputt_pp.byte"); A"on"; A"camlp4o"]);
+            flag ["compile"]                   (S [A"-I"; A (kpdir "")]);
+            flag ["link"; "native"; "program"] (S [A"-I"; A (kpdir ""); A"kaputt.cmx"]);
+          end
       | _ -> ()
   in
   dispatch
